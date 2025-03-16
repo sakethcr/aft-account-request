7510f99b27393edb6be6bfb6c671c92d119176a9
Saketh Chodimella <schodimella@channel4.co.uk>
2025-03-16 22:36:52 +0000

    accenture preprod account added

---
+++ terraform/Non-Production/account-request-avspreprod.tf
@@ -0,0 +1,35 @@
+module "account_request_avs_preprod" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "saketh.chodimella.avspreprod@panaceatechsolutions.com"
+    AccountName               = "avs-preprod-account"
+    ManagedOrganizationalUnit = "Non-Production (ou-t0vx-1putc8kg)" 
+    SSOUserEmail              = "newuser@panaceatechsolutions.com"
+    SSOUserFirstName          = "Account"
+    SSOUserLastName           = "Manager"
+  }
+
+  account_tags = {
+    "Acc:Owner"       = "sakethcr@gmail.com"
+    "Acc:Division"    = "Accenture"
+    "Acc:Environment" = "Preprod"
+    "Acc:CostCenter"  = "123456"
+    "Acc:Vended"      = "true"
+    "Acc:DivCode"     = "102"
+    "Acc:BUCode"      = "ABC003"
+    "Acc:Project"     = "123456"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Saketh AFT Demo"
+    change_reason       = "Demo AWS Control Tower Account Factory for Terraform (AFT)"
+  }
+
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
+
+  account_customizations_name = "SANDBOX"
+}
